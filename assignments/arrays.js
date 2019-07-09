c891a82222f1c6a4631598ae4510e4156f4d03c4
Carlos <miklo.concepcion@gmail.com>
2019-07-09 17:12:10 -0500

    finished! lineecompleting stretch tasks

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -62,21 +62,62 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log('2011 Jeep Wrangler');
+ console.log('Car 33 is a ' + inventory[32].car_year + ' ' + inventory[32].car_make + ' ' + inventory[32].car_model + '.');
+
+        for (let i = 0; i < inventory.length; i++){
+          if (inventory[i].id === 33){
+            console.log('Car 33 is a ' + inventory[i].car_year + ' ' + inventory[i].car_make + ' ' + inventory[i].car_model +'.');
+          }
+        }
+
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 50
-console.log('Lincoln Town Car');
+let lastCar = '0'
+// console.log('Lincoln', 'Town Car');
+
+    for (let i = 0; i < inventory.length; i++){
+        if (inventory[i] === inventory[inventory.length - 1]){
+        console.log(inventory[i]);
+        }
+    }
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+
+    for (let i = 0; i < inventory.length; i++){
+    let models = inventory[i].car_model;
+    carModels.push(models);
+    carModels.sort();
+    console.log(carModels);
+    
+    }
+
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [1];
-console.log();
+let carYears = [];
+// console.log();
+for (let i = 0; i < inventory.length; i++){
+    //  console.log(inventory[i].car_year);
+     const year = inventory[i].car_year;
+    //  console.log(year);
+     carYears.push(year);
+     console.log(carYears);
+}
+
+function test() {
+        var sub_array = [];
+        var super_array = [];
+        for (var i = 1; i <= 3; i++) {
+            sub_array.push(i);
+            super_array.push(sub_array.slice(0));
+        }
+        alert(super_array);
+    }
+      
+    
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -85,8 +126,15 @@ console.log();
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
+let BMWAndAudi =[];
 console.log();
+
+  for (let i = 0; i < inventory.length; i++){
+      if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi'){
+        BMWAndAudi.push(inventory[i]);
+        console.log(BMWAndAudi);
+      }
+  }
 
 
 
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,43 @@ let myFunction = function () {
 console.log("Function was invoked!");
 };
 myFunction();
+// converted arrow function no parameter
+let myFunction = _ => "Function was invoked!"
+
+
+
+
 
 let anotherFunction = function (param) {
   return param;
 };
 anotherFunction("Example");
+// converted arrow function single parameter
+let anotherFunction = param => param
+
+
+
+
 
 let add = function (param1, param2) {
   return param1 + param2;
 };
-add(1,2);
+add(1,2); 
+// converted arrow function - 2 parameter
+let add = (param1, param2) =>  param1 + param2
+
+
+
+
 
 let subtract = function (param1, param2) {
   return param1 - param2;
 };
 subtract(1,2);
+// converted arrow function - 2 parameter
+let subtract = (param1, param2) => param1 - param2
 
+console.log(subtract(4,2))
 
 // Stretch
 
@@ -28,3 +49,10 @@ subtract(1,2);
 //   return num * 3;
 // });
 // console.log(triple);
+
+
+
+// questions. 
+// can i nest a function in a function. technically yes. because its a value. cannot be invoked when nested unless from an outside object.
+
+  
